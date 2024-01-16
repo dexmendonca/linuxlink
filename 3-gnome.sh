3a4d8e0416560b1a8da95d0cb3ab4617ef400598
André Mendonça e Silva <dexmendoncadev@gmail.com>
2024-01-16 13:25:46 -0300

    update script

--- 3-gnome.sh
+++ 3-gnome.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "3- Config gnome packages"
 sudo pacman -R totem gnome-music --noconfirm
-sudo echo "WaylandEnable=false" >> /etc/gdm/custom.conf
+echo "WaylandEnable=false" | sudo tee -a /etc/gdm/custom.conf
