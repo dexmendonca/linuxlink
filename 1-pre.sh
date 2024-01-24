c24838d92e262e0e1b6c6d140a728d9edb76993c
André Mendonça e Silva <dexmendoncadev@gmail.com>
2024-01-24 18:33:17 -0300

    update script

--- 1-pre.sh
+++ 1-pre.sh
@@ -4,9 +4,15 @@ echo "1- Installing Base packages"
 sudo pacman -R vim --noconfirm
 sudo pacman -S  nano base base-devel net-tools --noconfirm
 
-# mkdir ./tmp
-# cd ./tmp
-# git clone https://aur.archlinux.org/yay-bin yb
-# cd ./yb
-# makepkg -s
-# sudo pacman -U *.pkg.* --noconfirm
+mkdir ./tmp
+cd ./tmp
+git clone https://github.com/powerline/fonts.git fonts
+cd fonts
+sh install.sh
+cd ..
+git clone https://aur.archlinux.org/yay-bin yb
+cd ./yb
+makepkg -s
+sudo pacman -U *.pkg.* --noconfirm
+cd ..
+cd ..
--- 3-gnome.sh
+++ 3-gnome.sh
@@ -3,5 +3,9 @@ echo "3- Config gnome packages"
 sudo pacman -R totem gnome-music --noconfirm
 sudo cp ./file/gdm.conf  /etc/gdm/custom.conf
 
+sudo pacman -S nerd-fonts secrets lsd powerline-fonts
 sudo pacman -S gvfs-gphoto2 gvfs-google gvfs-smb gvfs-nfs gvfs-mtp gvfs-goa gvfs-afc gvfs --noconfirm
 sudo pacman -S gstreamermm gst-python gst-libav gstreamer gnome-video-effects lib32-gst-plugins-base lib32-gst-plugins-good lib32-gst-plugins-base-libs gst-plugins-base-libs gst-plugin-libcamera gst-plugins-bad-libs  gst-plugin-pipewire gst-plugins-espeak gst-plugin-opencv gst-plugins-good gst-plugins-ugly gst-plugin-qmlgl gst-plugins-base gst-plugin-msdk gst-plugins-bad gst-plugin-qml6 gst-plugin-gtk gst-plugin-qsv gst-plugin-wpe gst-plugin-va --noconfirm
+
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
